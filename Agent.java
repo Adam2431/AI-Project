fd90d84c45dffb879c29a0a4ec8a7d7ae8d99997
Adam Samy <83244653+Adam2431@users.noreply.github.com>
2023-10-25 00:01:58 +0300

    Added some functionality to the functions

--- Agent.java
+++ Agent.java
@@ -1,25 +1,39 @@
 public class Agent {
-    public void RequestFood(){
+    int food;
+    int materials;
+    int energy;
+    int prosperity;
 
+
+    public void RequestFood(int amount, int delay){
+        food--;
+        materials--;
+        energy--;
     }
 
-    public void RequestMaterials(){
-        
+    public void RequestMaterials(int amount, int delay){
+        food--;
+        materials--;
+        energy--;
     }
 
-    public void RequestEnergy(){
-        
+    public void RequestEnergy(int amount, int delay){
+        food--;
+        materials--;
+        energy--;
     }
 
     public void WAIT(){
+        food--;
+        materials--;
+        energy--;
+    }
+
+    public void BUILD1(int food, int materials, int energy, int prosperity){
         
     }
 
-    public void BUILD1(){
-        
-    }
-
-    public void BUILD2(){
+    public void BUILD2(int food, int materials, int energy, int prosperity){
         
     }
 }
